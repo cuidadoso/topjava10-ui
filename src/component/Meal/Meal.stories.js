35132575fd3480401fd470b2815d33734a9af3d2
cuidadoso <alexander_s@front.ru>
2017-03-28 21:35:30 +0200

    Add stories for User, Meal.

---
+++ src/component/Meal/Meal.stories.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alejandro on 28.03.2017.
+ */
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import Meal from './Meal';
+
+storiesOf('Meal', module)
+    .add('just component', () => (
+        <Meal/>
+    ));
---
+++ src/component/User/User.stories.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alejandro on 28.03.2017.
+ */
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import User from './User';
+
+storiesOf('User', module)
+    .add('just component', () => (
+        <User/>
+    ));
