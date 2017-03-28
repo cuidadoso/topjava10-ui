8a93333da49ae19b2e79e4a9812b36ea3b7bedd9
cuidadoso <alexander_s@front.ru>
2017-03-28 22:03:13 +0200

    Add react router in App. Add history.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes as pt } from 'react';
 import {Router, useRouterHistory} from 'react-router';
+import {createHistory} from 'history';
 
 import logo from './logo.svg';
 import './App.css';
@@ -10,20 +11,32 @@ class App extends Component {
     };
     static displayName = 'Topjava10App';
 
+    constructor(props) {
+        super(props);
+        const {initialState = {}} = props;
+        const {context = window.CDT_CONTEXT, servletPath = window.CDT_SERVLETPATH} = initialState;
+        const contextWithSlash = context ? `/${context}` : '';
+        this.history = useRouterHistory(createHistory)(
+            {basename: `${contextWithSlash}/${servletPath}`}
+        );
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        const { routes } = this.props;
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Router routes={routes} history={this.history} />
+            </div>
+        );
+    }
 }
 
 export default App;
