33e5b4b290df3f1015fd66bb09c707dedb18a8fe
cuidadoso <alexander_s@front.ru>
2017-03-29 16:33:12 +0200

    Add mocha and test example.

---
+++ test/mymodule.js
@@ -0,0 +1,6 @@
+/**
+ * Created by apyreev on 29-Mar-17.
+ */
+exports.fun = (name) => {
+    return 'Привет, ' + name + '!';
+};
---
+++ test/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by apyreev on 29-Mar-17.
+ */
+const assert = require('assert');
+const mymodule = require('./mymodule.js');
+
+describe('mymodule', () => {
+    describe('#fun()', () => {
+        it('должна приветствовать пользователя, имя которого передано как аргумент', () => {
+            assert.equal(mymodule.fun('Сергей'), 'Привет, Сергей!');
+        });
+    });
+});
